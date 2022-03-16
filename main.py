17a126b261ec026995d1a48f5565521b5aa3c08d
GiorgiaMusso <95913363+GiorgiaMusso@users.noreply.github.com>
2022-03-16 15:08:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#due interi
+i1 = 25
+i2 = 21
+#due float
+f1 = 3.14
+f2 = 4.05
+#due complessi
+c1 =complex(1,2)
+c2 = complex(3,7)
+#due stringhe
+s1 = "Python"
+s2 = "Trieste"
+#somma tra interi
+i = i1 + i2
+print(i)
+#divisione tra float
+f = f1/f2
+print (f)
+#differenza tra complessi
+c = c1 - c2
+print(c)
+#somma tra stringhe
+s = s1 + s2
+print(s)
+#operazioni booleane
+print(i1==i2)
+print(s1!=s2)
+#type casting esplicito
+s3 = str(f1)
+print(s2 + s3)
+#type casting implicito
+print(i1-f1)
